fee8bd58d2fa27bc030dc2fb8e4ec4459c0bc965
PowerCell12 <gumbal4e@gmail.com>
2025-04-06 20:34:18 +0300

    finished the design for profile page

--- src/components/ProfilePage/ProfilePageComponent.tsx
+++ src/components/ProfilePage/ProfilePageComponent.tsx
@@ -8,7 +8,7 @@ export default function ProfilePageComponent({user, setUser} : HeaderProps){
     return (
         <section className="ProfilePageComponent">
 
-            <aside>
+            <aside className="ProfilePageAccountManagment">
                 <h2>Account Managment</h2>
 
                 <img  src="/PersonDefault.png" alt="" />
@@ -27,36 +27,53 @@ export default function ProfilePageComponent({user, setUser} : HeaderProps){
 
             <main >
 
-                <h2>Profile Information</h2>
+                <h2 className="ProfilePageh2">Profile Information</h2>
 
                 <section className="ProfilePageProfileInformation">
-                    <h5>Username</h5>
-                    <p>{user.name}</p>
+                    <article>
+                        <h5>Username</h5>
+                        <p>{user.name == user.email ? "N/A" : user.name}</p>
+                    </article>
 
-                    <h5>Full Name</h5>
-                    <p>{user.fullName}</p>
+                    <article>
+                        <h5>Full Name</h5>
+                        <p>{user.fullName == null ? "N/A" : user.fullName}</p>
+                    </article>
 
-                    <h5>Account Type</h5>
-                    <p>{user.accountType}</p>
-
-                    <h5>Country</h5>
-                    <p>{user.country}</p>
                 </section>
 
+                <section className="ProfilePageProfileInformation">
+                    <article>
+                        <h5>Account Type</h5>
+                        <p>{user.accountType ? user.accountType : "N/A"}</p>
+                    </article>
 
-                <h3>Contact Info</h3>
-                <h5>Email</h5>
-                <p>{user.email}</p>
+                    <article>
+                        <h5>Country</h5>
+                        <p>{user.country == null ? "N/A" : user.country}</p>
+                    </article>
+
+                </section>
+
+                <h3 className="ProfilePageh3">Contact Info</h3>
+                <h5 className="ProfilePageh5">Email</h5>
+                <p className="ProfilePagep ProfilePageEmail">{user.email}</p>
 
 
-                <h3>About the User</h3>
+                <h3 className="ProfilePageh3">About the User</h3>
 
-                <h5>Bio</h5>
-                <p>{user.bio}</p>
+                <h5 className="ProfilePageh5">Bio</h5>
+                <p className="ProfilePagep ProfilePageBio">{user.bio == null ? "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit tempora pariatur eius, totam quisquam dolore nisi eligendi illo eos temporibus itaque harum obcaecati recusandae eum quod nihil quos sequi sit!" : user.bio}</p>
 
             </main>
 
-
+            <aside className="ProfilePageAccountSettings">
+                <h2>Account Settings</h2>
+                <section className="ProfilePageAccountSettingsSection">
+                    <Link id="ProfilePageLinkEdit" to="/profile" className="ProfilePageLink">Edit Profile</Link>
+                    <Link id="ProfilePageLinkDelete" to="/profile" className="ProfilePageLink">Delete Account</Link>
+                </section>
+            </aside>
 
 
         </section>
